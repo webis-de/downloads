f7437d21711b61e078e1a5c1e93d89426b803def
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2018-10-19 18:21:08 +0200

    Deleted scripts

--- lecturenotes/update-images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for unit_file in $(ls */unit-*.pdf);do
-  dir=${unit_file%.pdf}
-  unit=$(basename $dir)
-
-  rm -rf $dir
-  ./generate-browser-data.sh $unit_file $dir
-  rm $dir/unit.json
-done
